c934b8dee430c877409644977ca6eb0f7631d1fc
Trần Trương Văn <kingchenobama711@gmail.com>
2024-11-24 14:35:01 +0700

    google button

--- app/[locale]/(auth)/_components/login-form.tsx
+++ app/[locale]/(auth)/_components/login-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useTransition } from "react"
+import Script from "next/script"
 import { Link, useRouter } from "@/i18n/routing"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useQueryClient } from "@tanstack/react-query"
@@ -21,7 +22,18 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import { Icons } from "@/components/ui/icons"
 import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+
+interface WindowWithGoogle extends Window {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  google?: any
+}
+
+declare let window: WindowWithGoogle
+
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!
 
 function LoginForm() {
   const t = useTranslations("LoginPage")
@@ -70,38 +82,53 @@ function LoginForm() {
     })
   }
 
+  const handleGoogleLogin = () => {
+    if (!window.google) {
+      console.error("Google SDK not loaded")
+      return
+    }
+
+    const tokenClient = window.google.accounts.oauth2.initTokenClient({
+      client_id: GOOGLE_CLIENT_ID,
+      scope: "profile email",
+      callback: (response: { access_token: string }) => {
+        console.log({ access_token: response.access_token })
+      },
+    })
+
+    tokenClient.requestAccessToken()
+  }
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t("Email")}</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={pending}
-                  placeholder="Enter your email"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex flex-col gap-y-4">
+    <>
+      <Script src="https://accounts.google.com/gsi/client" async defer />
+      <Button
+        onClick={handleGoogleLogin}
+        variant="outline"
+        size="sm"
+        className="w-full"
+      >
+        <Icons.Google className="mr-2 size-4" />
+        Continue with Google
+      </Button>
+
+      <div className="flex items-center gap-x-2">
+        <Separator className="flex-1" />
+        or
+        <Separator className="flex-1" />
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
             control={form.control}
-            name="password"
+            name="email"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>{t("Password")}</FormLabel>
+                <FormLabel>{t("Email")}</FormLabel>
                 <FormControl>
                   <Input
                     disabled={pending}
-                    type="password"
-                    placeholder="Enter your password"
+                    placeholder="Enter your email"
                     {...field}
                   />
                 </FormControl>
@@ -109,30 +136,54 @@ function LoginForm() {
               </FormItem>
             )}
           />
-          <div className="flex flex-wrap justify-between gap-2 text-sm">
-            <div className="flex items-center gap-2">
-              <Checkbox id="remember" /> Remember me
+          <div className="flex flex-col gap-y-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("Password")}</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={pending}
+                      type="password"
+                      placeholder="Enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex flex-wrap justify-between gap-2 text-sm">
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" /> Remember me
+              </div>
+              <Link href="/forgot-password" className="hover:underline">
+                Forgot password?
+              </Link>
             </div>
-            <Link href="/forgot-password" className="hover:underline">
-              Forgot password?
-            </Link>
           </div>
-        </div>
-        <Button disabled={pending} type="submit" className="w-full">
-          {t("Login")} {pending && <Loader2 className="size-4 animate-spin" />}
-        </Button>
+          <Button disabled={pending} type="submit" className="w-full">
+            {t("Login")}{" "}
+            {pending && <Loader2 className="size-4 animate-spin" />}
+          </Button>
 
-        <div className="flex justify-between text-sm">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            New user?
-            <Link href="/register" className="text-foreground hover:underline">
-              Register Here
-            </Link>
+          <div className="flex justify-between text-sm">
+            <div className="flex items-center gap-1 text-muted-foreground">
+              New user?
+              <Link
+                href="/register"
+                className="text-foreground hover:underline"
+              >
+                Register Here
+              </Link>
+            </div>
+            <Link href={"#"}>Use As Guest</Link>
           </div>
-          <Link href={"#"}>Use As Guest</Link>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </>
   )
 }
 
--- app/[locale]/(auth)/_components/register-form.tsx
+++ app/[locale]/(auth)/_components/register-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useTransition } from "react"
+import Script from "next/script"
 import { Link, useRouter } from "@/i18n/routing"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Loader2 } from "lucide-react"
@@ -24,7 +25,18 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import { Icons } from "@/components/ui/icons"
 import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+
+interface WindowWithGoogle extends Window {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  google?: any
+}
+
+declare let window: WindowWithGoogle
+
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!
 
 function RegisterForm() {
   const t = useTranslations("RegisterPage")
@@ -69,73 +81,53 @@ function RegisterForm() {
     })
   }
 
+  const handleGoogleLogin = () => {
+    if (!window.google) {
+      console.error("Google SDK not loaded")
+      return
+    }
+
+    const tokenClient = window.google.accounts.oauth2.initTokenClient({
+      client_id: GOOGLE_CLIENT_ID,
+      scope: "profile email",
+      callback: (response: { access_token: string }) => {
+        console.log({ access_token: response.access_token })
+      },
+    })
+
+    tokenClient.requestAccessToken()
+  }
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="studentCode"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t("StudentCode")}</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={pending}
-                  placeholder="Enter your Student Code"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>{t("Email")}</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={pending}
-                  placeholder="Enter your Email"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input
-                  disabled={pending}
-                  type="password"
-                  placeholder="Enter your password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex flex-col gap-y-4">
+    <>
+      <Script src="https://accounts.google.com/gsi/client" async defer />
+      <Button
+        onClick={handleGoogleLogin}
+        variant="outline"
+        size="sm"
+        className="w-full"
+      >
+        <Icons.Google className="mr-2 size-4" />
+        Continue with Google
+      </Button>
+
+      <div className="flex items-center gap-x-2">
+        <Separator className="flex-1" />
+        or
+        <Separator className="flex-1" />
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
             control={form.control}
-            name="confirmPassword"
+            name="studentCode"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Confirm Password</FormLabel>
+                <FormLabel>{t("StudentCode")}</FormLabel>
                 <FormControl>
                   <Input
                     disabled={pending}
-                    type="password"
-                    placeholder="Enter your confirmed password"
+                    placeholder="Enter your Student Code"
                     {...field}
                   />
                 </FormControl>
@@ -143,29 +135,84 @@ function RegisterForm() {
               </FormItem>
             )}
           />
-          <div className="flex justify-between text-sm">
-            <div className="flex items-center gap-2">
-              <Checkbox id="remember" /> Remember me
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("Email")}</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={pending}
+                    placeholder="Enter your Email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={pending}
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col gap-y-4">
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={pending}
+                      type="password"
+                      placeholder="Enter your confirmed password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-between text-sm">
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" /> Remember me
+              </div>
+              <Link href={"/forgot-password"}>Forgot password</Link>
             </div>
-            <Link href={"/forgot-password"}>Forgot password</Link>
           </div>
-        </div>
-        <Button disabled={pending} type="submit" className="w-full">
-          {t("Register")}
-          {pending && <Loader2 className="size-4 animate-spin" />}
-        </Button>
+          <Button disabled={pending} type="submit" className="w-full">
+            {t("Register")}
+            {pending && <Loader2 className="size-4 animate-spin" />}
+          </Button>
 
-        <div className="flex justify-between text-sm">
-          <div className="flex items-center gap-1 text-muted-foreground">
-            Already a user?
-            <Link href="/login" className="text-foreground hover:underline">
-              Login Here
-            </Link>
+          <div className="flex justify-between text-sm">
+            <div className="flex items-center gap-1 text-muted-foreground">
+              Already a user?
+              <Link href="/login" className="text-foreground hover:underline">
+                Login Here
+              </Link>
+            </div>
+            <Link href={"#"}>Use As Guest</Link>
           </div>
-          <Link href={"#"}>Use As Guest</Link>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </>
   )
 }
 
