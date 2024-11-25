8b3c5a5bb29a1442af483ca23f6bd3313ddfdab5
Trần Trương Văn <kingchenobama711@gmail.com>
2024-11-25 10:52:10 +0700

    change middleware for i18n

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,10 @@ import { routing } from "./i18n/routing"
 export default createMiddleware(routing)
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(vi|en)/:path*"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 }
