5045bc3936b7be32fa7b2c72b686e4f143371216
DoanVietThanh <doanvietthanhhs@gmail.com>
2024-12-10 21:33:53 +0700

    fix: account layout

--- app/[locale]/(browse)/me/account/layout.tsx
+++ app/[locale]/(browse)/me/account/layout.tsx
@@ -1,7 +1,4 @@
-import React, { type ReactNode } from "react"
-
-import LeftSidebar from "@/app/[locale]/management/_components/left-sidebar"
-import ManagementNavbar from "@/app/[locale]/management/_components/management-navbar"
+import { type ReactNode } from "react"
 
 import HeaderTabAccount from "./_components/account-header-tab"
 
@@ -12,20 +9,10 @@ type AccountLayoutProps = {
 
 const AccountLayout = ({ children, params }: AccountLayoutProps) => {
   return (
-    <main className="relative bg-background">
-      <ManagementNavbar />
-      <div className="flex">
-        <LeftSidebar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-20 max-md:pb-14 sm:px-8">
-          <div className="mx-auto size-full max-w-[1400px]">
-            <div className="flex h-full flex-col gap-4 rounded-lg p-4 shadow-lg">
-              <HeaderTabAccount locale={params.locale} />
-              <div className="mt-6">{children}</div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </main>
+    <div className="flex h-full flex-col gap-4 rounded-lg p-4 shadow-lg">
+      <HeaderTabAccount locale={params.locale} />
+      <div className="mt-6">{children}</div>
+    </div>
   )
 }
 
