d2f7a05f39fbd0557f3b9eccd41e32314911a2fd
DoanVietThanh <doanvietthanhhs@gmail.com>
2024-12-17 01:20:51 +0700

    feat: sort, filter employees

--- actions/employees/change-status.ts
+++ actions/employees/change-status.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { revalidateTag } from "next/cache"
+import { auth } from "@/queries/auth"
 
 import { handleHttpError, http } from "@/lib/http"
 import { type ActionResponse } from "@/lib/types/action-response"
@@ -8,10 +9,18 @@ import { type ActionResponse } from "@/lib/types/action-response"
 export async function changeEmployeeStatus(
   employeeId: string
 ): Promise<ActionResponse<string>> {
+  const { getAccessToken } = auth()
+  console.log("🚀 ~ getAccessToken:", getAccessToken())
+  console.log(123)
   try {
     const { message } = await http.patch(
       `/api/employees/${employeeId}/status`,
-      {}
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${getAccessToken()}`,
+        },
+      }
     )
 
     revalidateTag("employees")
