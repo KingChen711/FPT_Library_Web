93cf7de4d66fec91bd2fd87207f93290eb7dc2e9
Trần Trương Văn <kingchenobama711@gmail.com>
2025-04-23 09:06:10 +0700

    add layout and loading for guides page

---
+++ app/[locale]/(browse)/guides/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {
+  children: React.ReactNode
+}
+
+function GuideLayout({ children }: Props) {
+  return <> {children}</>
+}
+
+export default GuideLayout
